e8a553384174d642bf23c5a7a0c7bf4bb9692abb
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-27 18:19:20 +0100

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""script that starts a Flask web application:
+web application must be listening on `0.0.0.0`, port `5000`
+Routes:
+/: display `“Hello HBNB!”`
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text variable
+(replace underscore _ symbols with a space )
+You must use the option `strict_slashes=False` in your route definition
+"""
+
+
 from flask import Flask
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_hbnb():
-    """print web"""
-    return 'Hello HBNB!'
+def hello():
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb')
 def hbnb():
-    """print web"""
-    return 'HBNB'
+    return "HBNB"
 
 
 @app.route('/c/<text>')
-def c_is_fun(text):
-    """print C followed by the value of the text variable"""
-    return 'C {}'.format(text.replace('_', ' '))
+def show_text(text):
+    return "C {}".format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
