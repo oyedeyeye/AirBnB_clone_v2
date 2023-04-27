b26aba6c23df5ed6662f73cbcfd97f5409c4a876
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-27 16:53:08 +0100

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
+"""script that starts a Flask web application:
+web application must be listening on `0.0.0.0`, port `5000`
+Routes:
+/: display `“Hello HBNB!”`
+/hbnb: display “HBNB”
+You must use the option `strict_slashes=False` in your route definition
+"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_hbnb():
-    """ Print Web """
-    return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Print Web """
-    return 'HBNB'
+    return "HBNB"
 
 
 if __name__ == '__main__':
