bce2c9bc5a7388f64f1f41ca9c77c6e8cd1aa246
Hezekiah Oyemade <oyedey@gmail.com>
2023-05-29 21:09:16 +0100

    task 0 with super user priviledge

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Setup a web servers for the deployment of web_static.
-apt update -y
-apt install -y nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo apt update -y
+sudo apt install -y nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "<!DOCTYPE html>
 <html>
   <head>
@@ -12,7 +12,7 @@ echo "<!DOCTYPE html>
     <p>ALX C9 Test</p>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
